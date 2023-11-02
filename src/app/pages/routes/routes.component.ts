9409e43d7f08cf215164c154a1c8970dc6ee8446
eze-mangalu <eze.mangalu@bigpayme.com>
2023-11-02 10:02:07 +0000

    Updated routes

--- src/app/pages/routes/routes.component.ts
+++ src/app/pages/routes/routes.component.ts
@@ -6,9 +6,70 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './routes.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RoutesComponent {
+  routingModule = `
+    import { NgModule } from '@angular/core';
+    import { Route, RouterModule } from '@angular/router';
+    import { IntroComponent } from './pages/intro/intro.component';
+    import { NotFoundComponent } from './pages/not-found/not-found.component';
 
+    const routes: Route[] = [
+      {
+        path: '',
+        component: IntroComponent,
+      },
+      {
+        path: 'components',
+        title: 'Components',
+        loadComponent: () =>
+          import('./pages/components/components.component').then(
+            (m) => m.ComponentsComponent,
+          ),
+      },
+      ...
+      {
+        path: '**',
+        title: '404: Not found',
+        component: NotFoundComponent,
+      },
+    ];
+
+    @NgModule({
+      imports: [RouterModule.forRoot(routes)],
+      exports: [RouterModule],
+    })
+    export class AppRoutingModule {}
+  `;
+  routeGuards = `
+    // Auth guard
+    import { Injectable } from "@angular/core";
+    import {
+      ActivatedRouteSnapshot,
+      CanActivate,
+      Router,
+      RouterStateSnapshot,
+    } from "@angular/router";
+
+    @Injectable()
+    export class AuthGuard implements CanActivate {
+      constructor(private authService: AuthService, private router: Router) {}
+
+      canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return true; // If we return false the route won't be accessible.
+      }
+    }
+    
+    // In the routing module
+    const routes: Routes = [
+      {
+        path: "/your-path",
+        canActivate: [AuthGuard],
+      },
+  `;
+
+  navigation = `
+      <a routerLink="/your-path">Navigate to path</a>
+  `;
 }
